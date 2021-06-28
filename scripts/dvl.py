9dbe8c5de931eef5890115e8707ab4bec6ec4c53
Evan Sneath <evan.sneath@orbitlogic.com>
2021-06-28 23:27:24 +0000

    changes before pulling

--- scripts/dvl.py
+++ scripts/dvl.py
@@ -2,41 +2,46 @@
 
 import rospy
 from geometry_msgs.msg import Vector3Stamped, Vector3
+from geometry_msgs.msg import TwistWithCovarianceStamped
 from nav_msgs.msg import Odometry
 import numpy as np
-from scipy.spatial.transform import Rotation as R
+# from scipy.spatial.transform import Rotation as R
 
 rospy.init_node("dvl")
-pub = rospy.Publisher("dvl", Vector3Stamped, queue_size=2)
+pub = rospy.Publisher("dvl", TwistWithCovarianceStamped, queue_size=2)
 
 rate = rospy.Rate(10)
 def callback(msg):
     global pub
     
-    q = msg.pose.pose.orientation
-    r = R.from_quat([q.x, q.y, q.z, q.w]).as_dcm().transpose()
+    # q = msg.pose.pose.orientation
+    # r = R.from_quat([q.x, q.y, q.z, q.w]).as_dcm().transpose()
 
-    v = msg.twist.twist.linear
-    v3 = np.array([v.x,v.y,v.z]).transpose()
+    # v = msg.twist.twist.linear
+    # v3 = np.array([v.x,v.y,v.z]).transpose()
 
     #Find velocity in body frame
-    dvl_v = r.dot(v3)
+    # dvl_v = r.dot(v3)
+    dvl_v = [0, 0, 0]
 
     #Add Noise
     dvl_x = dvl_v[0] + np.random.normal(0, scale=0.005)
     dvl_y = dvl_v[1] + np.random.normal(0, scale=0.005)
     dvl_z = dvl_v[2] + np.random.normal(0, scale=0.005)
 
+    new_vel = TwistWithCovarianceStamped()
+    new_vel.twist.twist.linear = Vector3(dvl_x, dvl_y, dvl_z)
+    cov = np.zeros((6,6))
+    cov[3:6,3:6] = -1 * np.eye(3)
+    new_vel.twist.covariance = list(cov.flatten())
 
-    new_vel = Vector3(dvl_x, dvl_y, dvl_z)
-    new_msg = Vector3Stamped()
-    new_msg.vector = new_vel
     #get header set up
-    new_msg.header.stamp = rospy.get_rostime()
-    new_msg.header.frame_id = rospy.get_namespace() + 'base_link'
+    new_vel.header.stamp = rospy.get_rostime()
+    new_vel.header.frame_id = 'base_link'
 
-    pub.publish(new_msg)
+    pub.publish(new_vel)
     rate.sleep()
 
-rospy.Subscriber("pose_gt", Odometry, callback, queue_size=1)
+# rospy.Subscriber("pose_gt", Odometry, callback, queue_size=1)
+rospy.Timer(rospy.Duration(0.5), callback)
 rospy.spin()
--- scripts/setpoint_visiter.py
+++ scripts/setpoint_visiter.py
@@ -17,20 +17,41 @@ rospy.Subscriber("strapdown/estimate", Odometry, callback)
 rospy.wait_for_message("strapdown/estimate", Odometry)
 
 
-os.system("rosservice call /bluerov2_4/uuv_control/arm_control {}")
+os.system("rosservice call /bluerov2_5/uuv_control/arm_control {}")
 print("armed...")
 
+starting_x = 0
+starting_y = 0
+depth = 1.0
+
 current_index = 0
-setpoints = [[8, -3.2, 1.0], [13.5, -7.0, 1.0], [13.5, 4.0, 1.0]]
+# setpoints = [[starting_x, starting_y+5, depth], [starting_x+5, starting_y+5, depth],[starting_x+5, starting_y, depth], [starting_x, starting_y, depth]]
+setpoints = [[starting_x+5, starting_y, depth], [starting_x+5, starting_y+5, depth],[starting_x, starting_y+5, depth], [starting_x, starting_y, depth]]
 
-heading_setpoint = 0.0
-depth_setpoint = 1.0
+# New program
+while 1:
+    heading_setpoint = 0.0
+    print(heading_setpoint)
+    depth_setpoint = 1.0
+    os.system("rosservice call /bluerov2_5/uuv_control/set_heading_depth 'heading: " + str(heading_setpoint) + "\ndepth: "+str(depth_setpoint) + "'") 
+    rospy.sleep(15)
+    heading_setpoint = 90.0
+    print(heading_setpoint)
+    os.system("rosservice call /bluerov2_5/uuv_control/set_heading_depth 'heading: " + str(heading_setpoint) + "\ndepth: "+str(depth_setpoint) + "'") 
+    rospy.sleep(15)
+    heading_setpoint = 180.0
+    print(heading_setpoint)
+    os.system("rosservice call /bluerov2_5/uuv_control/set_heading_depth 'heading: " + str(heading_setpoint) + "\ndepth: "+str(depth_setpoint) + "'") 
+    rospy.sleep(15)
+    heading_setpoint = 270.0
+    print(heading_setpoint)
+    os.system("rosservice call /bluerov2_5/uuv_control/set_heading_depth 'heading: " + str(heading_setpoint) + "\ndepth: "+str(depth_setpoint) + "'") 
+    rospy.sleep(15)
 
 
-os.system("rosservice call /bluerov2_4/uuv_control/set_heading_depth 'heading: " + str(heading_setpoint) + "\ndepth: "+str(depth_setpoint) + "'") 
 
 print("diving...")
-rospy.sleep(20)
+rospy.sleep(5)
 
 r = rospy.Rate(1)
 
@@ -42,18 +63,25 @@ while not rospy.is_shutdown():
     diff_x = x_target - x_current
     diff_y = y_target - y_current
     ori = np.arctan2(diff_y, diff_x)
+    print("Traversing to waypoint: {}, current position: {}".format(setpoints[current_index], [x_current, y_current]))
 
-    if np.abs(diff_x) < 1 and np.abs(diff_y) < 1:
+    if np.abs(diff_x) < 2 and np.abs(diff_y) < 2:
         current_index += 1
+        print("ADVANCING WAYPOINT")
+        if current_index == len(setpoints):
+            break
         continue
 
-    x_vel = 0.1*np.cos(ori)
-    y_vel = 0.1*np.sin(ori)
+    x_vel = 0.2*np.cos(ori)
+    y_vel = 0.2*np.sin(ori)
+    print([diff_x, diff_y])
 
     # transform ENU --> NED
-    x_vel, y_vel = y_vel, x_vel
+    # x_vel, y_vel = y_vel, x_vel
     z_vel = 0.0
-    heading_setpoint = 90 - (ori * (180/np.pi))
-    os.system("rosservice call /bluerov2_4/uuv_control/set_heading_velocity '{heading: " + str(heading_setpoint) + ", velocity: {x: "+str(y_vel) + ", y: " + str(x_vel) + ", z: " + str(z_vel) + "}}'") 
+    # heading_setpoint = 90 - (ori * (180/np.pi))
+    heading_setpoint = 0.0 #ori * (180/np.pi) + 90
+    os.system("rosservice call /bluerov2_5/uuv_control/set_heading_velocity '{heading: " + str(heading_setpoint) + ", velocity: {x: "+str(y_vel) + ", y: " + str(x_vel) + ", z: " + str(z_vel) + "}}'") 
     # rospy.sleep(10)
     r.sleep()
+os.system("rosservice call /bluerov2_5/uuv_control/set_heading_velocity '{heading: " + str(heading_setpoint) + ", velocity: {x: "+str(0.0) + ", y: " + str(0.0) + ", z: " + str(0.0) + "}}'") 
