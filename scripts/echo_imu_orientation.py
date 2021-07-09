1eb83af77f9f5d576f7f05284b8f935e1a61e6d6
Luke Morrissey <luke.morrissey@colorado.edu>
2021-07-09 19:45:59 +0000

    Add echo scripts for different IMU/odom messaging

--- scripts/echo_imu_orientation.py
+++ scripts/echo_imu_orientation.py
@@ -3,16 +3,16 @@
 import rospy
 import tf
 import numpy as np
-from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 
-def callback(odom):
-    pose = odom.pose.pose
+def callback(imu):
+    pose = imu
     roll, pitch, yaw = tf.transformations.euler_from_quaternion([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
 
     print('RPY: ({:0.2f}, {:0.2f}, {:0.2f})'.format(np.degrees(roll), np.degrees(pitch), np.degrees(yaw)))
 
 
 actor = ""
-rospy.init_node("odom_ori_echo", anonymous=True)
-rospy.Subscriber("{}/odometry/filtered/odom_gyro".format(actor), Odometry, callback)
+rospy.init_node("imu_echo")
+rospy.Subscriber("{}/imu/data".format(actor), Imu, callback)
 rospy.spin()
